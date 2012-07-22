584f5c0a4a4d699c5278244a32fd25c84f9f36ce
minowu <minowu@foxmail.com>
2012-07-22 12:03:26 +0800

    fixed hide bug for high image.

--- turnjs/app/pack.js
+++ turnjs/app/pack.js
@@ -24,8 +24,8 @@
             // 如果存在高分辨率图片
             // 插入zoomlay中，并等待加载完毕后替换low图片
             if(highSrc){
-                var high = '<img id="zoomlayHigh" src="'+ highSrc +'">';
-                zoomlayPage.append(high).hide();
+                var high = '<img id="zoomlayHigh" src="'+ highSrc +'" style="display: none;">';
+                zoomlayPage.append(high);
 
                 var $high = $('#zoomlayHigh'),
                     $low = $('#zoomlayLow');
